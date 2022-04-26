1f3ce8c2e2179e0818132e198044cb575c7146a3
Thiago Marinho <tgmarinho@gmail.com>
2022-04-25 23:59:56 -0400

    setup infura and create a polygon account

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
+require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
 
-const projectId = 'xxx'
+const projectId = PROCESS.env.PROJECT_INFURA_ID;
+
+const fs = require('fs')
+// p-key.txt contains the private key of the account metamask
+const keyData = fs.readFileSync('./p-key.txt', {
+  encoding: 'utf8', flag: 'r'
+})
+
 
 
 module.exports = {
@@ -11,11 +19,11 @@ module.exports = {
     },
     mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
-      accounts: []
+      accounts: [keyData]
     },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${projectId}`,
-      accounts: []
+      accounts: [keyData]
     },
   },
   solidity: {
