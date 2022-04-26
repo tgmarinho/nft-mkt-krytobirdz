a2342fc1bd5e0b8cfa398cb0888e7e439347923e
Thiago Marinho <tgmarinho@gmail.com>
2022-04-25 23:30:24 -0400

    create infura project and setup polygon networks in the hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,30 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const projectId = 'xxx'
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337, // config standard
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: []
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${projectId}`,
+      accounts: []
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
 };
